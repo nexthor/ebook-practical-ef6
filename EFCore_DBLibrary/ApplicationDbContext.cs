1901b614015038a4b9604374ac81467935a5ad01
Nestor Mendoza <nestor.mendozam@gmail.com>
2022-06-16 17:51:04 -0600

    first commit

---
+++ EFCore_DBLibrary/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_DBLibrary
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() { }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
